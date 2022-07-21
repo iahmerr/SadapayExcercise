501703ce05824848b1e5cd1aceeff8240eae4712
Ahmer Hassan <ahmar@digitify.com>
2022-07-21 07:03:41 +0500

    Initial Commit

---
+++ SadapayExcercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SadapayExcercise
+//
+//  Created by Ahmer Hassan on 21/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
