dfec872ad7eabbd7fc9130afc2721a64aa55d94b
Ahmer Hassan <ahmar@digitify.com>
2022-07-21 16:58:41 +0500

    - Shimmering Effect is added in UIViewExtension

---
+++ SadapayExcercise/AppKit/Extension/UIView+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIView+Extension.swift
+//  SadapayExcercise
+//
+//  Created by Ahmer Hassan on 21/07/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func startShimmeringEffect() {
+        let gradientColorOne : CGColor = UIColor(white: 0.85, alpha: 1.0).cgColor
+        let gradientColorTwo : CGColor = UIColor(white: 0.95, alpha: 0.6).cgColor
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = self.bounds
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.colors = [gradientColorOne, gradientColorTwo,   gradientColorOne]
+        gradientLayer.locations = [0.0, 0.5, 1.0]
+        self.layer.addSublayer(gradientLayer)
+        
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [-1, -0.3, -0.5, 0]
+        animation.toValue = [1.0, 1.3, 1.5, 2]
+        animation.duration = 0.9
+        animation.repeatCount = .infinity
+        gradientLayer.add(animation, forKey: animation.keyPath)
+    }
+    
+    func stopShimmeringEffect() {
+        if let gradientLayer =  self.layer.sublayers?.first(where: { (layer) -> Bool in
+            return layer.isKind(of: CAGradientLayer.self)
+        })
+        {
+            gradientLayer.removeFromSuperlayer()
+        }
+        layer.mask = nil
+    }
+}
